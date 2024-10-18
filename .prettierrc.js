18379f00c872aee577ee1107817f9f347ac6c9bd
Oppossome <dsecutler@gmail.com>
2024-10-18 14:58:16 -0400

    Add CI

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,16 +2,13 @@
  * @type {import('prettier').Config}
  */
 export default {
-	"useTabs": true,
-	"semi": false,
-	"plugins": [
-		"prettier-plugin-svelte", 
-		"prettier-plugin-tailwindcss"
-	],
-	"overrides": [
+	useTabs: true,
+	semi: false,
+	plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+	overrides: [
 		{
-			"files": "*.svelte",
-			"options": { "parser": "svelte" }
-		}
-	]
+			files: "*.svelte",
+			options: { parser: "svelte" },
+		},
+	],
 }
