6a09d11f063f1660f9948ae7b3541abf73f4d959
Oppossome <dsecutler@gmail.com>
2024-10-18 11:57:55 -0400

    Initial Commit

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import eslintPluginSvelte from "eslint-plugin-svelte"
+import tsParser from "@typescript-eslint/parser"
+import svelteParser from "svelte-eslint-parser"
+
+export default [
+	// General
+	{ files: ["**/*.{js,mjs,cjs,ts}"] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		ignores: ["node_modules"],
+		rules: {
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{
+					argsIgnorePattern: "^_",
+					varsIgnorePattern: "^\\$\\$(Props|Events|Slots|Generic)$",
+				},
+			],
+		},
+	},
+	// Svelte
+	...eslintPluginSvelte.configs["flat/prettier"],
+	{
+		files: ["**/*.svelte", "*.svelte"],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: tsParser,
+			},
+		},
+	},
+]
